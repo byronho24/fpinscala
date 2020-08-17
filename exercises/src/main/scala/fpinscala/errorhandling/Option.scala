93af994d53c6d2445634fd09cc20826811495944
Byron Ho <byronho24@gmail.com>
2020-08-17 15:01:58 +1000

    After move

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,15 +4,33 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
+  // Apply f if Option is not None.
+  def map[B](f: A => B): Option[B] = this match {
+      case None => None
+      case Some(v) => Some(f(v))
+  }
 
-  def getOrElse[B>:A](default: => B): B = ???
+  // Apply f, which may fail, to the Option if not None
+  def flatMap[B](f: A => Option[B]): Option[B] = 
+      this.map(f).getOrElse(None)
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  // Get value inside Option if not None, else get something else
+  // B has to at least include A, since return value can be of type A (when Option is not None)
+  // default: => B means that only evaluate default if we need it - lazy evaluation
+  def getOrElse[B >: A](default: => B): B = this match {
+      case None => default
+      case Some(v) => v
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
-
-  def filter(f: A => Boolean): Option[A] = ???
+  // Returns this Option if this Option is not None, else returns the Option in the arg.
+  // ? When would this be useful?
+  def orElse[B >: A](ob: => Option[B]): Option[B] = 
+      this.map(Some(_)).getOrElse(ob)
+  // Turns Some into None if filter condition not met. If None already, return None
+  def filter(f: A => Boolean): Option[A] = {
+      val keep = this.map(f).getOrElse(false)
+      if (keep) this else None
+  }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -35,11 +53,26 @@ object Option {
     catch { case e: Exception => 43 }
   }
 
+  // When would the different methods on Option be useful?
+  // map: Proceed with the computation if an error has not occurred
+  //      This also acts to delay the error handling process
+  //      So (Something that might fail).map(Do the next thing)
+  //      is like saying "Do this; if this doesn't fail, do the next thing"
+  // flatMap: same as above, except the `next thing` might fail as well
+  // getOrElse: use this when you actually want to handle the error
+  // orElse: chain together two computations that can fail
+  //         i.e. (A).orElse(B) is like saying 
+  //              `if A fails, *try* B instead` (try because we don't know if B would succeed!)
+  // getOrElse can be used to turn None back to a plain old Exception - 
+  // should only do that if no reasonable program would be able to
+  // recover from the exception
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
 
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
